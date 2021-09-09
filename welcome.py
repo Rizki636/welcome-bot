297fea73ad41f30da67c298e2c7c859db1221ade
Rizki636 <76016674+Rizki636@users.noreply.github.com>
2021-09-09 09:15:46 +0800

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,44 +1,35 @@
-import request
-import get
-import os
+import time
+import asyncio
+import sys
+import random
+import threading
 
+from telethon import TelegramClient, events, utils
 
-token = os.environ['TOKEN']
-url = f'https://api.telegram.org/bot1903596942:AAG1ESoxiUWLV4DURH49giMQ8w9xb1r1qjI/'
+# gunakan api id dan hash punya anda sendiri, atau cari aja punya orang lain
 
+session_file = '/data/data/com.termux/files/home/sesisReply'  # bisa ditulis walau belum login asal punya akses write
+password = 'YOUR_PASSWORD'  # jika anda menerapkan two step verification
 
-otakBot = Flask(__name__)
-
-def update(update):
-  if 'new_chat_member' in str(update):
-     nama_grup = update['message']['chat']['title']
-     grup_id   = update['message']['chat']['id']
-     mem_baru  = update['message']['new_chat_member']['first_name']
-     teks      = f'Hai {mem_baru} !\nSelamat datang di Grup {nama_grup}'
-     kirim_pesan(grup_id,teks)
-  else:
-     # disini bisa kita tambahin kode lainnya
-     # misalnya membalas kalau ada oran yang ngechat ke bot kita 
-     # dan kreasikan sesuai imajinasi kalian :)
-     id = update['message']['chat']['id']
-     kirim_pesan(id,'halo, saya bot rizki')
-  
-def kirim_pesan(id,teks):
-  data = {
-  'chat_id':id,
-  'text':teks
-  }
-  get(url+'sendMessage',params=data)
-
-
-@otakBot.route('/',methods=['POST','GET'])
-def index():
-  if request.method == 'POST':
-     data_update = request.get_json()
-     update(data_update)
-     return "oke"
-  else:
-     return 'HALAMAN BOT PERTAMA SAYAHALAMAN INI BISA DI GANTI DENGAN KODE HTML'
+# Isi pesan
+message = "lagi off"
 
 if __name__ == '__main__':
-  otakBot.run(host='0.0.0.0',port=int(os.environ.get('PORT','5000')),debug=True)
+    # Create the client and connect
+    # use sequential_updates=True to respond to messages one at a time
+    client = TelegramClient(session_file, api_id=Config.APP_ID, api_hash=Config.API_HASH, sequential_updates=True)
+        
+        @client.on(events.NewMessage(incoming=True))
+    async def handle_new_message(event):
+        if event.is_private:  # only auto-reply to private chats
+            from_ = await event.client.get_entity(event.from_id)  # this lookup will be cached by telethon
+            if not from_.bot:  # don't auto-reply to bots
+                print(time.asctime(), '-', event.message)  # optionally log time and message
+                time.sleep(1)  # pause for 1 second to rate-limit automatic replies
+                await event.respond(message)
+
+
+    print(time.asctime(), '-', 'Auto-replying...')
+    client.start()
+    client.run_until_disconnected()
+    print(time.asctime(), '-', 'Stopped!')
